a69d63413dc5e211201327efb049e2ed90397f45
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 21:34:01 +0530

    File Updated

--- 1_Intro.dart
+++ 1_Intro.dart
@@ -2,3 +2,5 @@
 // It is an application programming language that is easy to learn ( hehe not so tbh)
 // It is versatile, easy to scale, and can be deployed anywhere and everywhere
 // Google is highly dependent on making very huge and large application that are supported by dart language; for example: darlang.org
+
+//  for learning flutter one must be familiar with Dart
