87a97962056f786607b67863833063013136436e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 21:34:46 +0530

    File Updated

--- 1_Intro.dart
+++ 1_Intro.dart
@@ -4,3 +4,4 @@
 // Google is highly dependent on making very huge and large application that are supported by dart language; for example: darlang.org
 
 //  for learning flutter one must be familiar with Dart
+// The fundamentals of dart are quite similar to other programming languages like JS, Java, or other C like langugae.
