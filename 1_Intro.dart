a0bffd5aa01c4ce0cb4908b93f426e66ced958fc
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-03 21:32:15 +0530

    File Updated

--- 1_Intro.dart
+++ 1_Intro.dart
@@ -1,2 +1,3 @@
 // Dart is a programming languages developed by google
 // It is an application programming language that is easy to learn ( hehe not so tbh)
+// It is versatile, easy to scale, and can be deployed anywhere and everywhere
