7c5323fc8cff4dcd5ff295d607a8afd692f632e9
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-04 21:04:34 +0530

    File Updated

--- 2_Installing_dart.dart
+++ 2_Installing_dart.dart
@@ -1,4 +1,4 @@
 // For our dart code to run we must make sure that our local system has dart installed or enabled in it
 // So today we will see on how to install dart on various platforms like Windows, mac, or Linux.
 
-// Installing 
+// Installing dart on windows
