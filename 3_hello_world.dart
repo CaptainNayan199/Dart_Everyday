2232c491fb0c159155d96734e496bf9f9db3506a
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 21:44:24 +0530

    File Updated

--- 3_hello_world.dart
+++ 3_hello_world.dart
@@ -1,2 +1,6 @@
 // Print function is used to print anything in dart language.
 // a code begins with a main function, it is the main function that dart looks for when executing our code
+
+void main (){ 
+   print("Hello Universe")
+}
