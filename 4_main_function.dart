b95131c79938a30ee61b1ab0e1882b27f1935e61
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 21:56:53 +0530

    File Updated

--- 4_main_function.dart
+++ 4_main_function.dart
@@ -9,4 +9,4 @@ void main() {
 // It is the entry point of our program
 // There must be only one main function in the program, multiple main function must be strictly prohibited.
 
-// SO yeah this much for today's class
+// SO yeah this much for today's class, this was all the basic abt dart, from tomorrow onward we will be talking abt variables, values, comments, operators, and many more 
