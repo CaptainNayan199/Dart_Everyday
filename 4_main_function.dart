fbe77bc327e0a21c72a55c223fafb378595d9d67
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-05 21:52:29 +0530

    File Updated

--- 4_main_function.dart
+++ 4_main_function.dart
@@ -1,2 +1,6 @@
 // As told earlier, any dart program should compulsory begin with a main function
 // user should define it and it is compulsion or else the code wont work
+
+void main() {
+  print('Hello, Dart');
+}
