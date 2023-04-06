38825cc30ca3aefb21f2fb1b75040398aa6a0209
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 22:40:04 +0530

    File Updated

--- 5_Variables.dart
+++ 5_Variables.dart
@@ -2,4 +2,4 @@
 // Variables are like a simple container where we store something, they are like our school bags, where we used to carry books, copies, pens, pencils, eraser, geometi boxes and many more
 // Variables in dart language are also like a container where we store multiple values with different data types
 
-// 
+// Defining variables - one of the simplest and 
