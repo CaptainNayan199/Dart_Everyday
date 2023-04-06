a18ccac97006333dc9ebd0b2e07288f3ee3f7027
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 22:46:05 +0530

    File Updated

--- 5_Variables.dart
+++ 5_Variables.dart
@@ -12,3 +12,4 @@ var message = "Hello world!"
 print(message);
 
 // Dart is a typed language. The type of the variable message is String. Dart can infer this type, so you did't have to explicitly define it as a String
+// Importantly, this variable must be a String forever. You cannot re-assign the variable as an integer. If you did want to create a variable that's more dynamic, you'd use the dynamic keyword. We'll see examples of that in a later lesson.
