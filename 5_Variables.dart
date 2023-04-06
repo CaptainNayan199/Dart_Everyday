3cf08a8198880736b95cbcd172730509c7b0ea87
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-06 22:37:11 +0530

    File Updated

--- 5_Variables.dart
+++ 5_Variables.dart
@@ -1,2 +1,2 @@
 // Variables are the building block for any programming language, and it is same for dart programming too
-// Variables are 
+// Variables are like a simple container where we store something
