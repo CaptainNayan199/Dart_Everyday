6e114178d8da8badd61ec1efb9844f667284310e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-07 23:33:28 +0545

    File Updated

---
+++ variables_2.dart
@@ -0,0 +1,8 @@
+// we have talked abt variables in short in previous session
+void main(){
+    for (int i=1; i<=5; i++){
+        print("Hello $i");
+    }
+}
+
+// This will print Hello1 to hello 5
