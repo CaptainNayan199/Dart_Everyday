1d0a9de68a6a3d371e0c8895932ea83a1132a231
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-09 23:05:57 +0545

    FIles updated

--- 7_comments.dart
+++ 7_comments.dart
@@ -1,3 +1,3 @@
 // Comments are nothing but just a simple line of codes or it may be anything that is not goinf to be executed by the compiler or the interpreter
 // Whatever is returned inside of the comment section is going to be neglected by the interpreter/compiler
-// If a person has a need of explaining code or writing something
+// If a person has a need of explaining code or writing something that must be ignored by the 
