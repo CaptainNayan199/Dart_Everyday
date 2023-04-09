44bfb37a5bb05c35dedd98fd4fc48ce1c11d97d4
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-09 23:08:14 +0545

    Files Updated

--- 7_comments.dart
+++ 7_comments.dart
@@ -4,4 +4,4 @@
 
 // There is only a single method of writing a comment in dart languages
 
-// Inline comments : it is a comment where two forward slashes are written at the beginning of the code to 
+// Inline comments : it is a comment where two forward slashes are written at the beginning of the code to ensure that it is a comment.
