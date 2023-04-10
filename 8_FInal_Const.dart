3574e5f5e106fa2f3de96d824656206761a8fa13
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-10 22:21:03 +0545

    Files Updated

--- 8_FInal_Const.dart
+++ 8_FInal_Const.dart
@@ -23,3 +23,15 @@ final namee = "Nayan"
 name = "Ujjwal"
 // This doesnot work
 
+// The only difference between final and const is that the const makes the variable constant from compile-time only.
+
+// There is a post on dart's website and it explains it pretty well.
+
+
+// So, what does this mean?
+// Const:
+// If the value you have is computed at runtime (new DateTime.now(), for example), you can not use a const for it. However, if the value is known at compile time (const a = 1;), then you should use const over final. There are 2 other large differences between const and final. Firstly, if you're using const inside a class, you have to declare it as static const rather than just const. Secondly, if you have a const collection, everything inside of that is in const. If you have a final collection, everything inside of that is not final.
+
+// Final:
+// final should be used over const if you don't know the value at compile time, and it will be calculated/grabbed at runtime. If you want an HTTP response that can't be changed, if you want to get something from a database, or if you want to read from a local file, use final. Anything that isn't known at compile time should be final over const.
+
