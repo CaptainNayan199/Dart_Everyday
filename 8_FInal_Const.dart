12d6c316610b6e45b31135c28b87e3440e0b1370
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-10 21:45:21 +0545

    Files Modified

--- 8_FInal_Const.dart
+++ 8_FInal_Const.dart
@@ -1,3 +1,4 @@
 // In previous session, we saw that we can define a variable with the use of var keyword in dart
 // But today we will be looking at two keyword that are very important to know about whether it is in dart or any other object oriented programming languages like Java, Python, etc
 // In place of var we can use keyword like const, and final
+// In simple terms, if a variable is defined using these kwywords, then their values can never be changed throughout the code
