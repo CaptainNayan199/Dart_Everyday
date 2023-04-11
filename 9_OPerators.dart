73451eafd30c21fc0bca6b41727bd546235f0a5b
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-04-11 22:05:08 +0545

    FIles Modified

--- 9_OPerators.dart
+++ 9_OPerators.dart
@@ -1,3 +1,8 @@
 // Dart supports all the operators you'd expect from a modern programming language.
 // There are every operators that are available in other programming languages like + , - , / , * , ++, --, %, etc. 
 // Lets see with examples :
+
+void main (){
+    var a = 10, b = 2;
+    var c = a +b
+}
